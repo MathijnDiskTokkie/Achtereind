613ba621c8f05d51a8c8cd972729e60b2ef57be7
MathijnDiskTokkie <msmits02@student.rocvantwente.nl>
2017-03-30 14:31:18 +0200

    Add files via upload

---
+++ Opdracht6/GrafisheRekenMachine.aspx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Rekenmachine : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnC_Click(object sender, EventArgs e)
+    {
+        LBereken.Text = "";
+        txtScherm.Text = "";
+        LGetal1.Text = "";
+    }
+
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "4";
+        }
+    }
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "1";
+        }
+    }
+
+    protected void btnPunt_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Contains(",") != true)
+        {
+            if (txtScherm.Text.Length == 0)
+            {
+                txtScherm.Text += "0";
+            }
+            else
+            {
+                txtScherm.Text += ",";
+            }
+        }
+    }
+
+    protected void txtScherm_TextChanged(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void btnPlus_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length > 0)
+        {
+            LGetal1.Text = txtScherm.Text;
+            txtScherm.Text = string.Empty;
+            LBereken.Text = "+";
+        }
+    }
+
+    protected void btnIs_Click(object sender, EventArgs e)
+    {
+        double G1 = Convert.ToDouble(txtScherm.Text);
+        double G2 = Convert.ToDouble(LGetal1.Text);
+        double Uitkomst = 0;
+        if (LBereken.Text == "-")
+        {
+            Uitkomst = G1 - G2;
+        }
+        if (LBereken.Text == "+")
+        {
+            Uitkomst = G1 + G2;
+        }
+        if (LBereken.Text == "*")
+        {
+            Uitkomst = G1 * G2;
+        }
+        txtScherm.Text = Uitkomst.ToString();
+    }
+
+    protected void btn0_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "0";
+        }
+    }
+
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "2";
+        }
+    }
+
+    protected void btn3_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "3";
+        }
+    }
+
+    protected void btn5_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "5";
+        }
+    }
+
+    protected void btn6_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "6";
+        }
+    }
+
+    protected void btn7_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "7";
+        }
+    }
+
+    protected void btn8_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "8";
+        }
+    }
+
+    protected void btn9_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "9";
+        }
+    }
+
+    protected void btnMin_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length > 0)
+        {
+            LGetal1.Text = txtScherm.Text;
+            txtScherm.Text = string.Empty;
+            LBereken.Text = "-";
+        }
+    }
+    protected void btnBack_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length > 0)
+        {
+            txtScherm.Text = txtScherm.Text.Remove(txtScherm.Text.Length -1);
+        }
+        // het weghalen van de 0 is niet gelukt
+    }
+
+    protected void btnM2_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length > 0)
+        {
+            LGetal1.Text = txtScherm.Text;
+            txtScherm.Text = string.Empty;
+            LBereken.Text = "*";
+        }
+    }
+}
---
+++ Opdracht6/GrafisheRekenmachine.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GrafisheRekenMachine.aspx.cs" Inherits="Rekenmachine" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            background-color: #003366;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:TextBox ID="txtScherm" runat="server" OnTextChanged="txtScherm_TextChanged" Width="180px"></asp:TextBox>
+    
+    </div>
+        <asp:Button ID="btn7" runat="server" Height="33px" Text="7" Width="33px" CssClass="auto-style1" ForeColor="White" OnClick="btn7_Click" BorderStyle="Ridge" />
+        <asp:Button ID="btn8" runat="server" Height="33px" Text="8" Width="33px" CssClass="auto-style1" ForeColor="White" OnClick="btn8_Click" BorderStyle="Ridge" />
+        <asp:Button ID="btn9" runat="server" Height="33px" Text="9" Width="33px" CssClass="auto-style1" ForeColor="White" OnClick="btn9_Click" BorderStyle="Ridge" />
+        <asp:Button ID="btnC" runat="server" Height="33px" OnClick="btnC_Click" Text="C" Width="33px" ForeColor="White" CssClass="auto-style1" BorderStyle="Ridge" />
+        <asp:Button ID="btnBack" runat="server" Height="33px" Text="Back" Width="38px" CssClass="auto-style1" ForeColor="White" OnClick="btnBack_Click" BorderStyle="Ridge" />
+        <p>
+            <asp:Button ID="btn4" runat="server" Height="33px" OnClick="Button5_Click" Text="4" Width="33px" ForeColor="White" CssClass="auto-style1" BorderStyle="Ridge" />
+            <asp:Button ID="btn5" runat="server" Height="33px" Text="5" Width="33px" CssClass="auto-style1" ForeColor="White" OnClick="btn5_Click" BorderStyle="Ridge" />
+            <asp:Button ID="btn6" runat="server" Height="33px" Text="6" Width="33px" CssClass="auto-style1" ForeColor="White" OnClick="btn6_Click" BorderStyle="Ridge" />
+            <asp:Button ID="btnPlus" runat="server" Height="33px" Text="+" Width="33px" OnClick="btnPlus_Click" ForeColor="White" CssClass="auto-style1" BorderStyle="Ridge" />
+            <asp:Button ID="btnM2" runat="server" Height="33px" Text="M2" Width="38px" CssClass="auto-style1" ForeColor="White" OnClick="btnM2_Click" BorderStyle="Ridge" />
+        </p>
+        <p>
+            <asp:Button ID="btn1" runat="server" Height="33px" OnClick="btn1_Click" Text="1" Width="33px" ForeColor="White" CssClass="auto-style1" BorderStyle="Ridge" />
+            <asp:Button ID="btn2" runat="server" Height="33px" Text="2" Width="33px" CssClass="auto-style1" ForeColor="White" OnClick="btn2_Click" BorderStyle="Ridge" />
+            <asp:Button ID="btn3" runat="server" Height="33px" Text="3" Width="33px" CssClass="auto-style1" ForeColor="White" OnClick="btn3_Click" BorderStyle="Ridge" />
+            <asp:Button ID="btnMin" runat="server" Height="33px" Text="-" Width="33px" CssClass="auto-style1" ForeColor="White" OnClick="btnMin_Click" BorderStyle="Ridge" />
+            <asp:Button ID="btnO1" runat="server" Height="33px" Text="O1" Width="38px" ForeColor="White" CssClass="auto-style1" BorderStyle="Ridge" />
+        </p>
+        <p>
+            <asp:Button ID="btn0" runat="server" Height="33px" Text="0" Width="70px" ForeColor="White" CssClass="auto-style1" OnClick="btn0_Click" BorderStyle="Ridge" />
+            <asp:Button ID="btnPunt" runat="server" Height="33px" OnClick="btnPunt_Click" ForeColor="White" Text="." Width="33px" CssClass="auto-style1" BorderStyle="Ridge" />
+            <asp:Button ID="btnIs" runat="server" Height="33px" Text="=" Width="33px" ForeColor="White" OnClick="btnIs_Click" CssClass="auto-style1" BorderStyle="Ridge" />
+            <asp:Button ID="btnO2" runat="server" Height="33px" Text="O2" Width="38px" ForeColor="White" CssClass="auto-style1" BorderStyle="Ridge" />
+        </p>
+        <asp:Label ID="LGetal1" runat="server" Text="Label" Visible="False" ForeColor="White" CssClass="auto-style1"></asp:Label>
+        <asp:Label ID="LBereken" runat="server" Text="Label" Visible="False" ForeColor="White" CssClass="auto-style1"></asp:Label>
+    </form>
+    </body>
+</html>
